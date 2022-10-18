3cee39d7e48f333ffc815d2e23e6e16dfba701fa
azkayef69 <103218602+azkayef69@users.noreply.github.com>
2022-10-19 00:54:19 +0600

    Update bdnumber.py

--- bdnumber.py
+++ bdnumber.py
@@ -2,7 +2,7 @@ import random
 codes=["017","019","013","014","015","016","018"]
 
 pl=int(input("Pass length(6-11): "))
-for i in range(10000):
+for i in range(100000):
     x=""
     while len(x)!=8:
       x=str(random.randint(00000000,99999999))
